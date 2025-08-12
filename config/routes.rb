428f78e68e6cb89ce61bbb6e63cd5568a9758cdf
Fallibi <119268948+Fallibi@users.noreply.github.com>
2025-08-11 19:31:22 -0700

    now can add articles and comments to blog and have them show up in the right places by adding a second model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
-
 Rails.application.routes.draw do
   # This sets the home page of your application to the articles index page.
   root "articles#index"
-
-  resources :articles
+  
+  resources :articles do
+    resources :comments
+  end
   
   # This is a default Rails route for health checks. You can leave it here.
   get "up" => "rails/health#show", as: :rails_health_check
